0ec1b2e4192d2436da8d58714875ea65a126d854
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2023-10-27 12:44:04 -0600

    Created vue app for admin panel

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'admin-panel')));
 
 app.listen(3000, () => console.log("App listening on port 3000"))
 
